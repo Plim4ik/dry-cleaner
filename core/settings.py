3c6e4dfd791602bb8bb854d5742b65d0bdff85fa
Fedor <hiblakk@gmail.com>
2024-12-06 14:08:17 +0300

    ✨ (base.html, settings.py): add main.css stylesheet and configure static/media file settings The main.css stylesheet is now included in the base HTML template to enhance the styling of the application. Additionally, the settings for static and media files have been configured, allowing for better organization and management of static assets and media uploads within the project. This improves the overall structure and accessibility of resources in the application.

--- core/settings.py
+++ core/settings.py
@@ -107,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
+
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
@@ -117,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'  # URL-префикс для обслуживания медиафайлов
+
+# Настройки статических файлов
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'  # URL-префикс для обслуживания статических файлов
+# Директории, в которых находятся статические файлы
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
